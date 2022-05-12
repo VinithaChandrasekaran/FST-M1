24a97752d957acc623d063016439c32031e48ce7
VinithaChandrasekaran <Sairam.vinitha410@gmail.com>
2022-05-13 01:23:48 +0530

    Add files via upload

---
+++ Cucumber/Activities/Activity 6.java
@@ -0,0 +1,30 @@
+package testRunner;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "C:\\Users\\Vinitha\\IdeaProjects\\cucumber\\src\\test\\java\\features",
+        glue = {"stepDefinitions"},
+        tags = "@activity1"
+       // tags = "@activity2"
+        //tags = "@activity3"
+        //tags = "@activity4"
+        //tags = "@activity5"
+       /* tags = "@SmokeTest",
+        plugin = {"pretty"},
+        monochrome = true */
+        /*tags = "@SmokeTest",
+        plugin = {"html: test-reports"},
+        monochrome = true */
+        /*tags = "@SimpleAlert",
+    plugin = {"pretty"},
+    monochrome = true*/
+        /*tags = "@SimpleAlert",
+        plugin = {"json: test-reports/json-report.json"},
+        monochrome = true*/
+)
+
+public class ActivitiesRunner {
+}
